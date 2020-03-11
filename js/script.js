e83a4f240b2e5d665addda035949a701668bc6a3
Kajanth Kumar <kajanthkumar@gmail.com>
2020-03-10 20:19:20 -0400

    initial commit

---
+++ js/script.js
@@ -0,0 +1,19 @@
+const slyGameApp = {
+//This is a game that highlights the programming problem known as the "knapsack problem". The user is tasked with selecting items from a set such that the total weight does not exceed a certain weight while attempting to maximize the total value from their selection.
+
+//JQuery shorthands
+
+//item is the specific item to be stolen
+$item: $(item);
+$submit: $(submit);
+};
+
+//slyGameApp methods
+slyGameApp.init = function () {
+    slyGameApp
+}
+
+//document ready
+$(document).ready(function () {
+    recipeApp.init();
+});
